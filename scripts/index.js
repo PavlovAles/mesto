9ee3fe7a6f5ae2e67ef605987e67d85b2bb42a41
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-03-26 13:30:06 +0300

    fix: исправить замечания 1 ревью в скрипте

--- scripts/index.js
+++ scripts/index.js
@@ -4,26 +4,31 @@ let infoAvocation = profile.querySelector(".profile__avocation");
 let btnEdit = profile.querySelector(".profile__btn-edit");
 
 let popup = document.querySelector(".popup");
-let inputName = popup.querySelector('.popup__form-item[id="name"]');
-let inputAvocation = popup.querySelector('.popup__form-item[id="avocation"]');
-let btnSave = popup.querySelector(".popup__btn-save");
+let popupForm = document.forms["popup-form"];
+let inputName = popup.querySelector("#name");
+let inputAvocation = popup.querySelector("#avocation");
 let btnClose = popup.querySelector(".popup__btn-close");
 
 btnEdit.addEventListener("click", function () {
-  popup.classList.add("popup_opened");
+  togglePopup();
+
   inputName.setAttribute("value", infoName.textContent.trim());
   inputAvocation.setAttribute("value", infoAvocation.textContent.trim());
 });
 
-btnSave.addEventListener("click", formSubmitHandler);
+popupForm.addEventListener("submit", formSubmitHandler);
 
-btnClose.addEventListener("click", function () {
-  popup.classList.remove("popup_opened");
-});
+btnClose.addEventListener("click", togglePopup);
+
+function togglePopup() {
+  popup.classList.toggle("popup_opened")
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
   infoName.textContent = inputName.value.trim();
   infoAvocation.textContent = inputAvocation.value.trim();
+
+  togglePopup();
 }
