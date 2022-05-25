d0be57db792e9431a8131aa94fc047c990c8501e
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-05-25 15:23:21 +0300

    refactor: внести изменения в index.js

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,98 @@
-import { Card } from '../components/Card.js';
-import { FormValidator } from '../components/FormValidator.js';
-import { showPopup, hidePopup } from '../utils/utils.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+import {
+  initialCards,
+  validatorSelectors,
+  placeForm,
+  placeBtnAdd,
+  placeBtnSave,
+  profileForm,
+  profileBtnEdit,
+  profileBtnSave,
+  profileInputName,
+  profileInputAvocation,
+} from '../utils/constants.js';
 
-const selectors = {
-  fieldsetSelector: '.popup__input-container',
-  inputSelector: '.popup__form-item',
-  submitButtonSelector: '.popup__btn-save',
-  inactiveButtonClass: 'popup__btn-save_disabled',
-  inputErrorClass: 'popup__form-item_type_error',
-  errorClass: 'popup__input-error_active'
-}
-
-const profile = document.querySelector('.profile');
-const profileName = profile.querySelector('.profile__name');
-const profileAvocation = profile.querySelector('.profile__avocation');
-const profileBtnEdit = profile.querySelector('.profile__btn-edit');
-const profilePopup = document.querySelector('.popup_contains_profile-form');
-const profileForm = profilePopup.querySelector('.popup__form');
-const profileFormValidator = new FormValidator(selectors, profileForm);
-const profileInputName = profileForm.querySelector('#name');
-const profileInputAvocation = profileForm.querySelector('#avocation');
-const profileBtnSave = profileForm.querySelector('.popup__btn-save');
-
-const placesContainer = document.querySelector('.elements__list');
-const placeBtnAdd = document.querySelector('.profile__btn-add');
-const placePopup = document.querySelector('.popup_contains_place-form');
-const placeForm = placePopup.querySelector('.popup__form');
-const placeFormValidator = new FormValidator(selectors, placeForm);
-const placeInputName = placeForm.querySelector('#place-name');
-const placeInputSrc = placeForm.querySelector('#place-src');
-const placeBtnSave = placeForm.querySelector('.popup__btn-save');
-
-const initialCards = [
+const placesContainer = new Section(
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    data: initialCards,
+    renderer: (item) => {
+      const card = new Card(
+        function() {
+          popupWithImage.open(this._placeName, this._placeSrc);
+        },
+        item.name,
+        item.link,
+        '#place-template'
+      );
+      const cardElement = card.generateCard();
+      placesContainer.setItem(cardElement);
+    }
   },
+  '.elements__list'
+);
+
+const popupWithCardForm = new PopupWithForm(
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    submitHandler: (evt) => {
+      evt.preventDefault();
+      popupWithCardForm._getInputValues();
+      const card = new Card(
+        function() {
+          popupWithImage.open(this._placeName, this._placeSrc);
+        },
+        popupWithCardForm._formValues['place-name'],
+        popupWithCardForm._formValues['place-src'],
+        '#place-template'
+      );
+      const cardElement = card.generateCard();
+      placesContainer.setItem(cardElement);
+      popupWithCardForm.close();
+      placeFormValidator.disableButton(placeBtnSave);
+    }
   },
+  '.popup_contains_place-form'
+);
+
+const popupWithUserForm = new PopupWithForm(
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    submitHandler: (evt) => {
+      evt.preventDefault();
+      popupWithUserForm._getInputValues();
+      userInfo.setUserInfo(popupWithUserForm._formValues.name, popupWithUserForm._formValues.avocation);
+      popupWithUserForm.close();
+      profileFormValidator.disableButton(profileBtnSave);
+    }
   },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
+  '.popup_contains_profile-form'
+)
 
-const profileFormSubmitHandler = function (evt) {
-  evt.preventDefault();
+const popupWithImage = new PopupWithImage('.popup_contains_big-img');
 
-  profileName.textContent = profileInputName.value.trim();
-  profileAvocation.textContent = profileInputAvocation.value.trim();
+const userInfo = new UserInfo( {nameSelector: '.profile__name', avocationSelector: '.profile__avocation'} );
 
-  hidePopup(profilePopup);
-  profileFormValidator.disableButton(profileBtnSave);
-};
+popupWithCardForm.setEventListeners();
+popupWithUserForm.setEventListeners();
+popupWithImage.setEventListeners();
 
-const placeFormSubmitHandler = function (evt) {
-  evt.preventDefault();
-
-  renderPlaceCard(placeInputName.value, placeInputSrc.value);
-  hidePopup(placePopup);
-  placeForm.reset();
-  placeFormValidator.disableButton(placeBtnSave);
-};
-
-const renderPlaceCard = function (placeName, placeSrc) {
-  const placeCard = new Card(placeName, placeSrc, '#place-template');
-  placesContainer.prepend(placeCard.generateCard());
-};
-
-profileBtnEdit.addEventListener('click', function () {
-  profileInputName.value = profileName.textContent.trim();
-  profileInputAvocation.value = profileAvocation.textContent.trim();
-
-  showPopup(profilePopup);
-});
-
-profileForm.addEventListener('submit', profileFormSubmitHandler);
-profileFormValidator.enableValidation();
-
-placeForm.addEventListener('submit', placeFormSubmitHandler);
+const placeFormValidator = new FormValidator(validatorSelectors, placeForm);
 placeFormValidator.enableValidation();
 
-placeBtnAdd.addEventListener('click', () => showPopup(placePopup));
+const profileFormValidator = new FormValidator(validatorSelectors, profileForm);
+profileFormValidator.enableValidation();
 
-initialCards.forEach((item) => renderPlaceCard(item.name, item.link));
+profileBtnEdit.addEventListener('click', function () {
+  let currentUserInfo = userInfo.getUserInfo();
+  profileInputName.value = currentUserInfo.name.trim();
+  profileInputAvocation.value = currentUserInfo.avocation.trim();
+
+  popupWithUserForm.open();
+});
+
+placeBtnAdd.addEventListener('click', () => popupWithCardForm.open());
+
+placesContainer.renderItems();
