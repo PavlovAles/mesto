32563fe3e451262286d76d097f0e3c88a500a20f
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-06-07 12:33:18 +0300

    feat: редактировать профиль через api

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,11 +1,16 @@
 export default class UserInfo {
-  constructor( {nameSelector, avocationSelector} ) {
+  constructor( {nameSelector, avocationSelector, avatarSelector} ) {
     this._nameElement = document.querySelector(nameSelector);
     this._avocationElement = document.querySelector(avocationSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
   }
 
   getUserInfo() {
     return {name: this._nameElement.textContent, avocation: this._avocationElement.textContent}
+  }
+
+  setAvatar(avatar) {
+    this._avatarElement.setAttribute('src', avatar);
   }
 
   setUserInfo(name, avocation) {
