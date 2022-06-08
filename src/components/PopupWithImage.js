bec557ce4a6ea722fb65845c4ad7eacd146ae85d
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-06-08 11:39:33 +0300

    fix: искать элементы картинки и описания в конструкторе popupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,10 +1,16 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._img = this._popup.querySelector('.popup__img');
+    this._caption = this._popup.querySelector('.popup__img-caption');
+  }
+
   open({name, link}) {
-    this._popup.querySelector('.popup__img').setAttribute('src', link);
-    this._popup.querySelector('.popup__img').setAttribute('alt', name);
-    this._popup.querySelector('.popup__img-caption').textContent = name;
+    this._img.setAttribute('src', link);
+    this._img.setAttribute('alt', name);
+    this._caption.textContent = name;
 
     super.open();
   }
